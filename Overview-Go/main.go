6550a5898321d51c8e763cea56974f7e72216865
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2024-12-28 23:55:11 -0300

    Second programm in Go

---
+++ Overview-Go/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world.")
+
+	var whatToSay string
+	var i int 
+
+	whatToSay = "Goodbye, cruel world."
+
+	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to", i)
+
+	whatWasSaid := saySomething()
+
+	fmt.Println("The function returned", whatWasSaid)
+}
+
+
+func saySomething() string {
+	return "something"
+}
