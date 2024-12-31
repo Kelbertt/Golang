a2e0ac6a6b2042d92426150f18778528a882f4e9
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2024-12-30 22:32:47 -0300

    manipulating string and using type struct

---
+++ Types-Structs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+var s = "seven"
+
+type User struct {
+	FirstName 	string
+	LastName 	string
+	PhoneNumber string
+	Age 		int
+	BirthDate 	time.Time
+}
+func main() {
+
+	user := User {
+		FirstName: "Kaio",
+		LastName: "Rangel",
+	}
+
+	log.Println(user.FirstName, user.LastName, "Birthdate", user.BirthDate )
+	
+
+	var s2 = "five"
+
+	log.Println("s is", s)
+	log.Println("s2 is", s2)
+
+	saySomething("xxx")
+
+}
+
+func saySomething(s string) (string, string) {
+	log.Println("s from the func is", s)
+	return s, "World"
+}
