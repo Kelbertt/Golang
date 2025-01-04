050001e7f308d6c1712dcbc762ffa51c869964d4
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2025-01-03 23:33:49 -0300

    Maps with int

--- Maps-Slices/main.go
+++ Maps-Slices/main.go
@@ -14,4 +14,11 @@ func main() {
 
 	log.Println(myMap["other-dog"])
 	log.Println(myMap["dog"])
+
+	myMap2 := make(map[string]int)
+
+	myMap2["First"] = 1
+	myMap2["Second"] = 2
+
+	log.Println(myMap2["First"], myMap2["Second"])
 }
