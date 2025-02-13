5dd629ed54e3a303d886d17a14a9428dbef1fa4b
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2025-02-13 01:17:34 -0300

    add dog info in the func main, add func of Says() and NumberofLegs to the dog, add func PrintInfo out the main to print the info of the dog

--- Interfaces/main.go
+++ Interfaces/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Animal interface {
 	Says() 	string
 	NumberofLegs() int
@@ -14,6 +16,25 @@ type Gorilla struct {
 	Name 	string
 	Color 	string
 }
-func main() {
 
+func PrintInfo( a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberofLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+func (d *Dog) NumberofLegs() int {
+	return 4
+}
+
+
+func main() {
+	dog := Dog{
+		Name:  "Buddy",
+		Breed: "German Shephered",
+	}
+
+	PrintInfo(&dog)
 }
