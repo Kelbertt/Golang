5ce4396de725173172565b520376c3ec4e766de8
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2025-02-13 00:53:56 -0300

    Finish the training of Loops & Range using type struct to register the info of users that was distributed in 3 using append, in sequence a for loop without i , only _ with l := range users

--- Loops/main.go
+++ Loops/main.go
@@ -25,4 +25,20 @@ func main() {
 	for animalType, animal := range animalsmap {
 		log.Println(animalType, animal)
 	}
+
+	type User struct {
+		FirstName 	string
+		LastName 	string
+		Email 		string
+		Age 		int
+	}
+
+	var users	[]User
+	users = append(users, User{"Kaio", "Rangel", "kaiomontecristo1@gmail.com", 22})
+	users = append(users, User{"Leticia", "Marins", "lelemarinslopes@gmail.com", 22})
+	users = append(users, User{"Karol", "Rangel", "karolmontecristo1@gmail.com", 17})
+
+	for _, l := range users {
+		log.Println(l.FirstName, l.LastName, l.Email, l.Age)
+	}
 }
