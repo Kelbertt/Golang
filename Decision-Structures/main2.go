b77e2bdc31202efb74707a2ab62ea74fa58fe1b6
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2025-02-12 23:49:47 -0300

    More of decision structures, in this time using switch/case

---
+++ Decision-Structures/main2.go
@@ -0,0 +1,26 @@
+package main
+
+import 
+	"log"
+
+
+func main2() {
+
+	myVar := "cat"
+
+	switch myVar {
+	case "cat":
+		log.Println("cat is set to cat")
+	
+	case "dog":
+		log.Println("cat is set to dog")
+
+	case "fish":
+		log.Println("cat is set to fish")	
+	
+	default:
+		log.Println("cat is set to something else")
+	}
+
+	
+}
