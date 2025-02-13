9aa0cfbb8a2673bb30af3b0afaa61228fef18f7a
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2025-02-13 00:43:32 -0300

    Others forms to use loops with for with a range

--- Loops/main.go
+++ Loops/main.go
@@ -4,7 +4,25 @@ import "log"
 
 func main() {
 
-	for i := 0; i <= 10; i++ {
+	for i := 0; i <= 5; i++ {
 		log.Println(i)
+	}
+
+	animals := []string{"dog", "fish", "cat", "horse", "cow"}
+
+	for i, animal := range animals {
+		log.Println(i, animal)
+	}
+
+	for _, animal := range animals {
+		log.Println(animal)
+	}
+
+	animalsmap := make(map[string]string)
+	animalsmap["dog"] = "Fluffy"
+	animalsmap["cat"] = "Doja"
+
+	for animalType, animal := range animalsmap {
+		log.Println(animalType, animal)
 	}
 }
